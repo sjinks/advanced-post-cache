bfa94a96ab60c886ff763cad14ca3b7b33a303a4
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-06-24 10:03:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ Description: Cache post queries. Based on Advanced Post Caching by Automattic.
 Version: 1.0
 Author: Volodymyr Kolesnykov
 Author URI: https://wildwolf.name/
+Requires at least: 5.5
+Requires PHP: 7.4
+License: GPLv2
 */
 
 require_once __DIR__ . '/class-advanced-post-cache.php';
